ab84123943473d87a67fda652a3f1d2dc26e147b
赵博 <zhaobo@zhaobodeMac-mini.local>
2019-03-12 11:34:31 +0800

    react-apollo-app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Dog} from './Dog';
 
 class App extends Component {
   render() {
@@ -8,16 +9,14 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://www.apollographql.com/docs/react/recipes/testing.html"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn Apollo
+            <Dog name="Buck" />
           </a>
         </header>
       </div>
--- src/index.js
+++ src/index.js
@@ -3,8 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { MockedProvider } from 'react-apollo/test-utils';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// The component AND the query need to be exported
+import { GET_DOG_QUERY, Dog } from './Dog';
+
+const mocks = [
+  {
+    request: {
+      query: GET_DOG_QUERY,
+      variables: {
+        name: 'Buck',
+      },
+    },
+    result: {
+      data: {
+        dog: { id: '1', name: 'Buck', breed: 'bulldog' },
+      },
+    },
+  },
+];
+
+ReactDOM.render(<MockedProvider mocks={mocks} addTypename={false}>
+  <App />
+</MockedProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
